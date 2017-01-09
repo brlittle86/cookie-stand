6d62029b2170a2a72ece0cc8c7ddfa698217e016
Brandon Little <brlittle86@gmail.com>
2017-01-09 14:08:25 -0800

    basic literals for each location built

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 'use strict';
 
+var pike = {
+    minCus: 23,
+    maxCus: 65,
+    avgSale: 6.3,
+    locName: '1st and Pike'
+}
+
+var seaTac = {
+    minCus: 3,
+    maxCus: 24,
+    avgSale: 1.2,
+    locName: 'SeaTac Airport'
+}
+
+var seaCent = {
+    minCus: 11,
+    maxCus: 38,
+    avgSale: 3.7,
+    locName: 'Seattle Center'
+}
+
+var capHill = {
+    minCus: 20,
+    maxCus: 38,
+    avgSale: 2.3,
+    locName: 'Capitol Hill'
+}
+
+var alki = {
+    minCus: ,
+    maxCus: ,
+    avgSale: ,
+    locName: 'Alki'
+}
